9059b312aa6ea452523ed5a045fb21c53870b667
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-05-20 05:56:38 +0200

    Added symlink to barman command. corrected some parameter names. in manifests/install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,19 +3,21 @@
 # Installs barman from pure repo
 class pure_barman::install
 (
-  $pg_data_dir = $pure_repmgr::pg_data_dir,
-  $pg_xlog_dir = $pure_repmgr::pg_xlog_dir,
 )
 {
 
   package {$pure_barman::params::barman_package:
     ensure => 'installed',
+  } ->
+
+  file { '/usr/local/bin/barman':
+    ensure => '/usr/pgpure/barman/bin/barman',
   }
 
   file { $pure_barman::params::barman_data_tree:
-    ensure => 'directory',
-    owner  => 'barman',
-    group  => 'barman',
+    ensure => directory,
+    owner  => $pure_barman::params::barman_user,
+    group  => $pure_barman::params::barman_group,
     mode   => '0750',
   }
 
