006cc697494efccceadcf59476207a19765dfdfe
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-06-23 10:34:18 +0200

    Added a copyright statement to every manifest

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,20 @@
+# Copyright (C) 2017 Collaboration of KPN and Splendid Data 
+#
+# This file is part of puppet_pure_barman.
+#
+# puppet_pure_barman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# puppet_pure_barman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with puppet_pure_barman.  If not, see <http://www.gnu.org/licenses/>.
+
 # == Class: pure_barman::config
 #
 # Configure a barman server to backup postgres database nodes
